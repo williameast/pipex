3a2c72905564465968c2c78d66e7c44b57b8bf09
William East <weast@c4c4c1.42berlin.de>
2025-03-05 14:56:36 +0100

    added libft submodule

--- src/pipex.c
+++ src/pipex.c
@@ -6,15 +6,18 @@
 /*   By: weast <weast@student.42berlin.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 10:53:12 by weast             #+#    #+#             */
-/*   Updated: 2025/03/05 11:57:18 by weast            ###   ########.fr       */
+/*   Updated: 2025/03/05 14:54:10 by weast            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+#include "libft.h"
+#include "ft_printf.h"
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
     if (argc == 2)
-		printf("you HAVE NEVER typed in: %s\n", argv[1]);
+		ft_printf("you HAVE NEVER typed in: %s\n", argv[1]);
+
     return 0;
 }
