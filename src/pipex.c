fc42b8e4866778e29311fe31220ea4b8d3e31b86
William East <weast@c4c4c1.42berlin.de>
2025-03-05 16:42:57 +0100

    added docs folder

--- src/pipex.c
+++ src/pipex.c
@@ -6,18 +6,6 @@
 /*   By: weast <weast@student.42berlin.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 10:53:12 by weast             #+#    #+#             */
-/*   Updated: 2025/03/05 14:54:10 by weast            ###   ########.fr       */
+/*   Updated: 2025/03/05 16:39:35 by weast            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "pipex.h"
-#include "libft.h"
-#include "ft_printf.h"
-#include <stdio.h>
-
-int main(int argc, char *argv[]) {
-    if (argc == 2)
-		ft_printf("you HAVE NEVER typed in: %s\n", argv[1]);
-
-    return 0;
-}
